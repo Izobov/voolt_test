08d31b12354128f6e2f9c1491239414e8ab00662
Ivan Izobov <izobov9@gmail.com>
2023-08-04 12:37:03 +0300

    [dev] Combobox component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,109 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	/** @type {import('./$types').PageData} */
+	import img from '$lib/images/left-side.png';
+	import logo from '$lib/images/voolt-logo.png';
+	import ComboBox from './ComboBox.svelte';
+	export let data;
+</script>
+
+<div class="wrapper theme">
+	<div class="img-wrapper" style="background-image: url({img})" />
+	<div class="content-wrapper">
+		<img src={logo} alt="logo" class="logo" />
+
+		<div class="form-wrapper">
+			<h1 class="title">What type of business is the website for?</h1>
+			<form class="form">
+				<ComboBox />
+			</form>
+		</div>
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		width: 100vw;
+		height: 100vh;
+		overflow: hidden;
+	}
+	:global(*) {
+		box-sizing: border-box;
+	}
+	:global(.theme) {
+		--gradient-mesh: radial-gradient(
+				108.2% 133.05% at 10.18% -60.65%,
+				rgba(245, 91, 20, 0.16) 0%,
+				rgba(245, 91, 20, 0) 100%
+			),
+			radial-gradient(
+				135.94% 86.02% at -21.43% 2.07%,
+				rgba(251, 188, 4, 0.12) 0%,
+				rgba(255, 210, 10, 0) 100%
+			),
+			radial-gradient(
+				107.19% 126.26% at 100% 117.34%,
+				rgba(128, 0, 255, 0.08) 0%,
+				rgba(128, 0, 255, 0) 100%
+			),
+			#f9fafa;
+		--blue-primary: #4253ff;
+		--base-grey-100: #202123;
+		--base-grey-90: #2e2f38;
+		--base-grey-40: #d8dbde;
+		--base-grey-30: #ecedef;
+		--base-grey-20: #f4f4f5;
+		--base-grey-10: #f9fafa;
+
+		--text-color: var(--base-grey-100);
+
+		--background: #fff;
+		--box-shadow: 0px 0px 0px 2px #e2ebfd;
+		--border-radius: 4px;
+	}
+	.wrapper {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		max-width: 100%;
+		color: var(--text-color);
+		font-family: Basier Circle;
+	}
+	.img-wrapper {
+		flex-grow: 1;
+		width: 50%;
+		background-repeat: no-repeat !important;
+		background-size: cover !important;
+		background: var(--gradient-mesh);
+	}
+	.content-wrapper {
+		width: 50%;
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 35px 100px;
+		background-color: var(--background);
+	}
+	.form-wrapper {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: center;
+	}
+	.logo {
+		height: 32px;
+		width: 110px;
+	}
+	.title {
+		font-weight: 600;
+		font-size: 32px;
+		max-width: 420px;
+	}
+
+	.form {
+		display: flex;
+		flex-direction: column;
+		gap: 40px;
+	}
+</style>
---
+++ src/routes/ComboBox.svelte
@@ -0,0 +1,164 @@
+<script>
+	// @ts-nocheck
+
+	import search_icon from '$lib/icons/search.png';
+	import arrow_down from '$lib/icons/nav-arrow-down.png';
+	import search_err from '$lib/icons/search-fast-itemm-1.png';
+
+	export let placeholder = 'Search';
+	const data = [
+		{ id: '1', value: 'Accounting' },
+		{ id: '2', value: 'Appliance Installation' },
+		{ id: '3', value: '3' },
+		{ id: '4', value: '4' }
+	];
+	let value = '';
+	let active = false;
+	let node;
+
+	$: filtered = data.filter((i) =>
+		value ? i.value.toLowerCase().includes(value.toLowerCase()) : i
+	);
+
+	function outsideHandle(e) {
+		if (node && !node.contains(e.target)) {
+			active = false;
+		}
+	}
+</script>
+
+<svelte:body on:click={outsideHandle} />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="wrapper" bind:this={node}>
+	<div class="select" class:active on:click={() => (active = true)}>
+		<img src={search_icon} alt="search" class="icon" />
+		<input type="text" {placeholder} class="input" bind:value />
+		<img src={arrow_down} alt="arrow down" class="icon" />
+	</div>
+	{#if active}
+		<div class="popup">
+			{#each filtered as { id, value: v } (id)}
+				<div class="option" on:click={() => (value = v)}>
+					{v}
+				</div>
+			{:else}
+				<div class="empty">
+					<div class="empty-content">
+						<div class="empty-icon">
+							<img src={search_err} alt="search icon" class="search-error-icon" />
+						</div>
+						<p class="empty-text">
+							Oops... We can’t find similar service in our list. Try another keyword to search
+						</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+	{/if}
+</div>
+
+<style>
+	.wrapper {
+		position: relative;
+		width: 100%;
+		overflow: visible;
+	}
+	.select {
+		width: 100%;
+		display: flex;
+		padding: 10px 16px;
+		align-items: center;
+		gap: 12px;
+		height: 44px;
+
+		border-radius: var(--border-radius);
+		border: 1px solid var(--base-grey-40);
+		background: var(--background);
+	}
+
+	.active {
+		border: 1px solid var(--blue-primary);
+		box-shadow: var(--box-shadow);
+	}
+
+	.icon {
+		width: 20px;
+		height: 20px;
+		cursor: pointer;
+	}
+
+	.input {
+		border: none;
+		flex-grow: 1;
+		outline: none;
+	}
+
+	.popup {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		gap: 2px;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--base-grey-30);
+		background: var(--background);
+		box-shadow: -10px 4px 24px 0px rgba(0, 0, 0, 0.08);
+		position: absolute;
+		flex-shrink: 0;
+		left: 0;
+		z-index: 2;
+		max-height: 258px;
+		height: fit-content;
+		overflow-y: auto;
+	}
+	.option {
+		padding: 10px 12px;
+		cursor: pointer;
+		text-transform: capitalize;
+	}
+
+	.option:hover {
+		background-color: var(--base-grey-10);
+	}
+
+	.empty {
+		width: 100%;
+		height: 100%;
+		height: 200px;
+		padding: 40px 0px;
+	}
+	.empty-content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 20px;
+	}
+
+	.empty-icon {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		display: flex;
+		flex-shrink: 0;
+		justify-content: center;
+		align-items: center;
+		background: var(--base-grey-10);
+	}
+	.search-error-icon {
+		width: 28px;
+		height: 28px;
+		border-radius: 50%;
+		color: var(--base-grey-20);
+	}
+
+	.empty-text {
+		font-size: 16px;
+		line-height: 24px;
+		letter-spacing: -0.16px;
+		text-align: center;
+		color: var(--base-grey-90);
+		max-width: 360px;
+		margin: 0;
+	}
+</style>
